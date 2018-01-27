38dd67830b3b6090ab7262eda25a63406b6d8827
Kalen Hammann <kalen7@gmail.com>
2018-01-27 15:18:52 -0500

    Link the position of the hands to the current time

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ let sec = date.getSeconds();
 console.log ("Hour: " + hr + " Minute: " + min + " Seconds: " + sec);
 
 
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 267;
+let hrPosition = (hr*360/12) + (min*(360/60)/12) + (sec*(360/60)/12);
+let minPosition = (min*360/60) + (sec*(360/60)/60);
+let secPosition = sec*360/60;
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
 MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
