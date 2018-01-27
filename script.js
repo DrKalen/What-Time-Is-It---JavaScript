8bdfa697eaa81ed753c428d94818b179b267700e
Kalen Hammann <kalen7@gmail.com>
2018-01-27 17:01:35 -0500

    Add comments on altgernative solutions to the "zero rewind" effect. Currently using smoother animation/less accuracy.

--- script.js
+++ script.js
@@ -2,32 +2,30 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+/* for jerkier animation but better accuracy from using Date instead of depending on the browser
+not to throttle the JavaScript tring to rerun the function every second
+move this code to inside the function and comment out the 3 indicated lines of code. the function. */ 
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log ("Hour: " + hr + " Minute: " + min + " Seconds: " + sec);
 
+
+let hrPosition = (hr*360/12) + (min*(360/60)/12);
+let minPosition = (min*360/60) + (sec*(360/60)/60);
+let secPosition = sec*360/60;
+/* End of code to move inside the function */
 
 function runTheClock() {
 
-/* for smoother operation but inaccuracy from depending on the browser
-not to throttle the JavaScript tring to rerun the function every second
-move this code to above the function. */ 
-    var date = new Date();
-    console.log(date);
-    let hr = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
-    console.log ("Hour: " + hr + " Minute: " + min + " Seconds: " + sec);
+/* for jerkier animation but greater accuracy move the above code 
+inside the function and acomment the next 3 lines. */
 
-
-    let hrPosition = (hr*360/12) + (min*(360/60)/12);
-    let minPosition = (min*360/60) + (sec*(360/60)/60);
-    let secPosition = sec*360/60;
-/* End of code to move above the function */
-
-/* for smoother animation, move the above code outside the function
-and uncomment the next 3 lines. */
-
-//    hrPosition = hrPosition+(3/360);
-//    minPosition = minPosition+(6/60);
-//    secPosition = secPosition+6;
+   hrPosition = hrPosition+(3/360);
+   minPosition = minPosition+(6/60);
+   secPosition = secPosition+6;
 
     HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
     MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
