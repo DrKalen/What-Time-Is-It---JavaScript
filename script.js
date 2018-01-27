a7f26dd7aee33dfbfee97448b6b5760e8c7c8fc1
Kalen Hammann <kalen7@gmail.com>
2018-01-27 14:51:04 -0500

    Get the date and time and split it into hours, minutes and seconds

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
+var date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
+console.log ("Hour: " + hr + " Minute: " + min + " Seconds: " + sec);
+
+
 let hrPosition = 20;
 let minPosition = 130;
 let secPosition = 267;
